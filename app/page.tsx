32b7eaebe77696bee591237f678d51cfff2404ef
Ana Labeca <anafrost01@gmail.com>
2024-07-05 16:03:55 +0100

    feat(home): ✨ update home component to use Navlinks

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-import { Link, Button } from '@nextui-org/react';
+import {
+  Link,
+  Button,
+  Navbar,
+  NavbarContent,
+  NavbarItem,
+} from '@nextui-org/react';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col'>
-      <header className='bg-slate-900'>
-        <p className='text-slate-50 p-6'>Home</p>
-        <Button
-          href='/login'
-          as={Link}
-          color='primary'
-          variant='solid'
-        >
-          Log In
-        </Button>
-      </header>
+      <Navbar>
+        <NavbarContent justify='end'>
+          <NavbarItem>
+            <Button href='/login' as={Link} color='primary' variant='solid'>
+              Log In
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+      <h1>Home</h1>
     </main>
   );
 }
