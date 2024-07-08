8c15e5580407cbca63e05eeb8eac8c8460ec6365
Ana Labeca <anafrost01@gmail.com>
2024-07-08 11:38:04 +0100

    feat(login): ✨ add form validation rules and display error msg

--- app/login/components/login-form.tsx
+++ app/login/components/login-form.tsx
@@ -2,17 +2,19 @@
 
 import { Button, Input } from '@nextui-org/react';
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { userAuthSchema, UserAuthSchemaType } from '@/schemas/user-auth';
 
 export default function LoginForm() {
   const {
     register,
     handleSubmit,
     reset,
-    formState: { isLoading },
-  } = useForm({mode: 'onChange'});
+    formState: { errors, isLoading, isValid },
+  } = useForm<UserAuthSchemaType>({ mode: 'onChange', resolver: zodResolver(userAuthSchema) });
 
-  const onSubmit = handleSubmit(data => {
-    console.log("data", data);
+  const onSubmit = handleSubmit((data) => {
+    console.log('data', data);
     reset();
   });
 
@@ -25,6 +27,8 @@ export default function LoginForm() {
         labelPlacement='outside'
         placeholder='you@example.com'
         required
+        isInvalid={!!errors.email}
+        errorMessage={errors.email?.message}
       />
       <Input
         {...register('password')}
@@ -33,11 +37,11 @@ export default function LoginForm() {
         labelPlacement='outside'
         placeholder='Enter your password'
         required
+        isInvalid={!!errors.password}
+        errorMessage={errors.password?.message}
       />
 
-
-
-      <Button type='submit' isDisabled={isLoading}>
+      <Button type='submit' isDisabled={!isValid} isLoading={isLoading}>
         {isLoading ? 'Submitting...' : 'Submit'}
       </Button>
     </form>
